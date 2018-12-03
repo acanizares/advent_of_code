cc094791a6a6a9cc264a2472d88f971da4792789
Ana Canizares <ana.canizares@jambit.com>
2018-12-03 15:55:01 +0100

    day2.2

--- day02.py
+++ day02.py
@@ -1,8 +1,11 @@
+from typing import List
+
 file = "input02.txt"
 
 with open(file, "rt") as f:
     ids = f.readlines()
 
+# Part 1
 
 doubles = 0
 triples = 0
@@ -24,3 +27,34 @@ for id in ids:
 print(f"Number of doubles: {doubles}")
 print(f"Number of triples: {triples}")
 print(f"Checksum: {doubles*triples}")
+
+# Part 2
+
+
+def check(x: str, y: str) -> bool:
+    """
+    Whether two strings differ at most at one position
+    """
+    if not x:
+        return not y
+    x0, x_tail = x[0], x[1:]
+    y0, y_tail = y[0], y[1:]
+    return (x_tail == y_tail) or (x0 == y0 and check(x_tail, y_tail))
+
+
+def find_ids(ids: List[str]) -> str:
+    for i, x in enumerate(ids[:-1]):
+        for y in ids[i + 1:]:
+            found = check(x, y)
+            if found:
+                print(f"The found ids: {x}, {y}")
+                common_sub = ""
+                for i, c in enumerate(x):
+                    if y[i] == c:
+                        common_sub += c
+                return(common_sub)
+    raise ValueError("No correct ids were found")
+
+
+res = find_ids(ids)
+print(f"The common substring: {res}")
